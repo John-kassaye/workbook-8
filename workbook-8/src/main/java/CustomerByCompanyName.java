5b31c27372bc94518bad8da4c02ea05f6a393c9f
Yohannes Kassaye <jmule1567@gmail.com>
2025-06-10 10:19:29 -0700

    Add monday exercise

---
+++ workbook-8/src/main/java/CustomerByCompanyName.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class CustomerByCompanyName {
+    public static void main(String[] args) throws SQLException {
+        // 1. connect
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", args[0], args[1]);
+
+        // 2. run query
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM northwind.Customers WHERE CompanyName LIKE ?;");
+        preparedStatement.setString(1, "%" + getUserInput() + "%");
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        // 3. process results
+        while(resultSet.next()) {
+            System.out.println("\nCustomer ID: " + resultSet.getString("CustomerID"));
+            System.out.println("Contact name: " + resultSet.getString("ContactName"));
+        }
+
+        // 4. close all resources
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+    }
+
+    public static String getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What company are you looking for?");
+        return scanner.nextLine();
+    }
+}
